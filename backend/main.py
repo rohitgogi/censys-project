82370e53ca518e13890ba87e4795dc2311469ad6
rohitgogi <rohit.gogi12@gmail.com>
2025-09-27 17:16:01 -0400

    added deployment to CORS

--- backend/main.py
+++ backend/main.py
@@ -94,6 +94,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
+        "https://censys-project.vercel.app",
 
     ],
     allow_credentials=True,
